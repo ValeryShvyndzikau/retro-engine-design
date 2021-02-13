2ab3ef8e1f82c4127446e242fbe8e81dcc27b45e
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2021-02-13 12:35:49 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -45,4 +45,6 @@ interface CalculationResultNested {
 4. DynamoService "insertItem" has optional "sortKey" param, but "getItem" recieves only "pk"
   TODO: clarify in docs wheather it's OK or NOT!!!
 
+5. TODO: EACH STEP DATA FROM DB & DATA TO DB !!!
+
 */
